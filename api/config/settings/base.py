22917346482adce00ecd5ae506c8ef98b6e0a1e7
MileCode <86170555+DeisyMilenaMar@users.noreply.github.com>
2024-12-26 12:03:14 -0500

    Update base.py

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -257,6 +257,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_USE_TLS = True
-MAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
